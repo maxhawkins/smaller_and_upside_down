b371cf9c204d52fd1a05b1326f4def5ce2aab745
Max Hawkins <maxhawkins@gmail.com>
2015-06-19 08:10:39 +0800

    add video and player

---
+++ www/app.js
@@ -0,0 +1,33 @@
+var playButton = document.getElementById('play-button');
+var overlay = document.getElementById('player-overlay');
+var closeButton = document.getElementById('close-button');    
+
+// Load the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'EyQce3w-NIA',
+    playsinline: 0, // full screen on iOS
+  });
+}
+
+function showPlayer() {
+  overlay.className = 'active';
+  player.playVideo();
+}
+
+function hidePlayer() {
+  overlay.className = '';
+  player.stopVideo();
+}
+
+playButton.addEventListener('click', showPlayer, false);
+overlay.addEventListener('click', hidePlayer, false);
+closeButton.addEventListener('click', hidePlayer, false);
